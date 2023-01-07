ffeb32b9d818a1f2d45ee2939c3eebfa4a855425
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-07 11:34:41 +0600

    Adding and updating a movie rating

--- movie/serializers.py
+++ movie/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from movie.models import Movie, Review
+from movie.models import Movie, Review, Rating
 
 
 class MovieListSerializers(serializers.ModelSerializer):
@@ -56,3 +56,19 @@ class MovieDetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Movie
         exclude = ('draft',)
+
+
+class CreateRatingSerializer(serializers.ModelSerializer):
+    """ Добавление рейтинга пользователем """
+
+    class Meta:
+        model = Rating
+        fields = ("star", "movie")
+
+    def create(self, validated_data):
+        rating = Rating.objects.update_or_create(
+            ip=validated_data.get('ip', None),
+            movie=validated_data.get('movie', None),
+            defaults={'star': validated_data.get('star')}
+        )
+        return rating
