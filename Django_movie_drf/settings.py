e8509bffd68aae80f1c71f60bc1a56179b9029c7
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-08 11:07:46 +0600

    Adding CORS

--- Django_movie_drf/settings.py
+++ Django_movie_drf/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'ckeditor_uploader',
     'djoser',
     'drf_yasg',
+    'corsheaders',
 
     'django_filters',
 
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,6 +134,11 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
