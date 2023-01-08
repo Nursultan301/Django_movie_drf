48510ed6aa153aa9f2cf295a2da335f62630c310
Nursultan301 <abykeevnursultan14@gmail.com>
2023-01-08 11:39:42 +0600

    Pagination

--- Django_movie_drf/settings.py
+++ Django_movie_drf/settings.py
@@ -216,7 +216,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2
 }
 # smtp
 # EMAIL_USE_TLS = True
--- movie/service.py
+++ movie/service.py
@@ -1,6 +1,23 @@
 from django_filters import rest_framework as filters
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from movie.models import Movie
+
+
+class PaginationMovies(PageNumberPagination):
+    page_size = 3
+    max_page_size = 1000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
 
 
 def get_client_ip(request):
